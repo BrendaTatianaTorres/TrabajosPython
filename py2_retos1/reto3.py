13bbae145debc7c587a26143356c543ec2ef9073
BrendaTatianaTo <>
2021-04-22 19:42:09 +0000

    Reto 3

---
+++ py2_retos1/reto3.py
@@ -0,0 +1,11 @@
+a = 5
+b = 3
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+print ('Dados los números',a, 'y' , b, 'se realizarán las operaciones básicas') 
+
+print (' Suma:', suma,'\n','Resta: ', resta,'\n','Multiplicación:', multiplicacion,'\n','Division:', division )
