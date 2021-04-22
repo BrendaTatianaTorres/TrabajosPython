4656eb6c89ae0d542ab8b32ef659a3455f3c5ee1
BrendaTatianaTo <>
2021-04-22 19:52:25 +0000

    Reto3 SM

--- py2_retos1/reto3.py
+++ py2_retos1/reto3.py
@@ -6,6 +6,6 @@ resta = a - b
 multiplicacion = a * b
 division = a / b
 
-print ('Dados los números',a, 'y' , b, 'se realizarán las operaciones básicas') 
+print ('Dados los números',a, 'y' , b, 'se realizarán las operaciones básicas:') 
 
 print (' Suma:', suma,'\n','Resta: ', resta,'\n','Multiplicación:', multiplicacion,'\n','Division:', division )
