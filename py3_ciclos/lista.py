f1100ceaeada93efe483b2fe8db5061ca4d44b24
BrendaTatianaTo <>
2021-04-27 22:42:12 +0000

    Sumatoria

---
+++ py3_ciclos/lista.py
@@ -0,0 +1,19 @@
+A = [2,4,6,8,1,1]
+B = [1,3,5,7,9,9]
+C = [0,0,0]
+
+def llenar_C (A,B):
+  n = len(A)//2
+  for i in range(n):
+    Ai = A[i+1]**2
+    Bi = B[2*i]
+    mult = Ai*Bi
+    suma = mult + B[n+i]
+    C[i] = suma
+  resultado = C
+  print ('C =', resultado)
+  print('\n')
+
+print('\n')
+print('A continuacion verá el resultado de el arreglo propuesto ')
+llenar_C (A,B)
