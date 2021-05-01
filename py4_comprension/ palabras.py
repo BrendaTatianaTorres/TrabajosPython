126589381e717cd12f83763e88dba07c28cda649
BrendaTatianaTo <>
2021-05-01 04:36:11 +0000

    palabras

--- py4_comprension/ palabras.py
+++ py4_comprension/ palabras.py
@@ -1,11 +1,9 @@
-A = ['cama', 'ropa', 'color', 'cafe', 'almohada']
+A = [' cama ', ' ropa ', ' color ', ' cafe ', ' almohada ']
 
 n=len(A)
-for i in range(n):
-    palabra = A[i]
-    rima = [palabra[i] for i in range (n) if 'a' in palabra[-1]] 
-print (rima)
 
+rima = [A[i] for i in range (n) if 'a ' in A[i]]
+print (rima)
 
 
       
