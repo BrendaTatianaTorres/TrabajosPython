070372e4e103737337dfc1b8f0db5ff9fe45c108
BrendaTatianaTo <81332596+BrendaTatianaTorres@users.noreply.github.com>
2021-08-11 22:42:27 +0000

    Inicio

---
+++ py5_matplotlib/plot_list.py
@@ -0,0 +1,10 @@
+A = []
+
+def llenar_A (num):
+  while  
+    
+
+print('Digite el primer número de la lista')
+num = input ('Escriba el número del que quiere ver la tabla ')
+num=int(num)
+llenar_A(num)
